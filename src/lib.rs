00c91a13bc05b7f31b0f4a3c995cb5293e1da4ae
Michael Hancock <220162+gringasalpastor@users.noreply.github.com>
2025-03-12 13:21:42 -0600

    Update mule-map to used NonZero and clean up docs (#6)
    * Initial design for mule-map2, that will replace current version
    
    * Add checks for size limits
    
    * Start to add entry api for mule_map2.
    
    * add the rest of the entry api, and update bench.rs. merge new version. Removed old mule-map.
    
    * Update README.md
    
    * Update docs
    
    * initial iterators.rs code

--- src/lib.rs
+++ src/lib.rs
@@ -1,45 +1,63 @@
 //! <p align="center">
-//!  <img
-//!   src="https://raw.githubusercontent.com/gringasalpastor/mule-map/refs/heads/master/assets/mule-with-map.png"
-//!   width="200"
-//!   height="200"
-//!   style="border-radius:50%" />
+//! <img src="https://raw.githubusercontent.com/gringasalpastor/mule-map/refs/heads/master/assets/mule-with-map.png" width="200" height="200"
+//! style="border-radius:50%" />
 //! </p>
 //!
-//! [`MuleMap`] is a hybrid between a [`HashMap`](std::collections::HashMap) and a lookup table. If a key is between  [`TABLE_MIN_VALUE`](MuleMap)
-//! and [`TABLE_MAX_VALUE`](MuleMap), then the value will be stored directly in the lookup table (keys must be integers)
-//! at `table[key - TABLE_MIN_VALUE]`, instead of the slower [`HashMap`](std::collections::HashMap). Benchmarks start to show speed improvements
-//! starting when  ~50% of the key accesses are in the lookup table. Performance is almost identical to [`HashMap`](std::collections::HashMap) when
-//! less than 50%  [`MuleMap`] tries to match the API of the standard library `HashMap` when possible.
+//! # `MuleMap<🫏,🗺>`
+//! [![ci](https://github.com/gringasalpastor/mule-map/actions/workflows/ci.yml/badge.svg)](https://github.com/gringasalpastor/mule-map/actions/workflows/ci.yml)
+//! [![](https://docs.rs/mule-map/badge.svg)](https://docs.rs/mule-map)
+//! [![Version](https://img.shields.io/crates/v/mule-map.svg?style=flat-square)](https://crates.io/crates/mule-map)
+//!
+//! `MuleMap` is a hybrid between a `HashMap` and a lookup table. It improves performance for frequently accessed keys in a known range. If a key (integer) is in the user specified range, then its value will be stored directly in the lookup table. Benchmarks (using random selection) start to show speed improvements when about 50% of the key accesses are in the lookup table. Performance is almost identical to `HashMap` with less than 50%. `MuleMap` tries to match the API of the standard library `HashMap` - making it a drop-in replacement for `HashMap`.
 //!
 //! ## Example
 //!
+//!
 //! ```rust
 //! use mule_map::MuleMap;
-//!
+//! use std::num::NonZero;
 //! type Hash = fnv_rs::FnvBuildHasher;  // Use whatever hash function you prefer
-//! let mut mule_map = MuleMap::<u32, usize, Hash>::new();
 //!
+//! // Using Entry API
+//! let mut mule_map = MuleMap::<u32, usize, Hash>::new();
 //! assert_eq!(mule_map.get(5), None);
 //! let entry = mule_map.entry(5);
 //! entry.or_insert(10);
 //! assert_eq!(mule_map.get(5), Some(&10));
+//!
+//! // Using NonZero and bump
+//! let mut mule_map_non_zero = MuleMap::<u32, NonZero<i32>, Hash>::default();
+//!
+//! mule_map_non_zero.bump_non_zero(10);
+//! mule_map_non_zero.bump_non_zero(10);
+//! mule_map_non_zero.bump_non_zero(999_999);
+//! mule_map_non_zero.bump_non_zero(999_999);
+//!
+//! assert_eq!(mule_map_non_zero.get(10), NonZero::<i32>::new(2).as_ref());
+//! assert_eq!(mule_map_non_zero.get(999_999),NonZero::<i32>::new(2).as_ref());
 //! ```
 //!
-//! ## Limitations
+//! ## Highlights
 //!
-//!  - Only supports keys that are primitive integer types ([`u8`], [`u16`], [`u32`], [`u64`], [`u128`], ~~[`usize`]~~,
-//!     [`i8`], [`i16`], [`i32`], [`i64`], [`i128`], and ~~[`isize`]~~).
-//!  - Does not currently support automatically converting enum's with primitive representations.
-//!  - Currently the type of a const generic can't depend on another generic type argument, so [`TABLE_MIN_VALUE`](MuleMap) and
-//!     [`TABLE_MAX_VALUE`](MuleMap) can't use the same type as the key. Because of this, I am using [`i128`], but that means
-//!     we can't represent values near [`u128::MAX`]. Hopefully having frequent keys near [`u128::MAX`] is extremely rare.
-//!  - Currently requires `std`
+//!  - All primitive integer types are supported for keys (`u8`, `u16`, `u32`, `u64`, `u128`, `usize`, `i8`, `i16`, `i32`, `i64`, `i128`, and `isize`).
+//!  - All corresponding `NonZero<T>` types are supported for keys.
+//!  - `NonZero<T>` key types take advantage of the niche optimizations (guaranteed by the rust compiler) by being stored as an `Option<NonZero<T>>`. This is used by `bump_non_zero()` to directly cast `Option<NonZero<T>>` to it's underlying integer type (using bytemuck - **no unsafe code**) and directly incrementing its value. See [benchmarks](#benchmarks) for details.
+//!  - *NOTE*: Currently the type of a const generic can't depend on another generic type argument, so `TABLE_MIN_VALUE` can't use the same type as the key. Because of this, I am using `i128`, but that means we can't represent values near `u128::MAX`. Hopefully having frequent keys near `u128::MAX` is extremely rare.
 //!
-//! ## Benchmarks
+//! ## <a name="benchmarks"></a> Benchmarks
 //!
 //! ![violin](https://raw.githubusercontent.com/gringasalpastor/mule-map/refs/heads/master/assets/violin.svg)
 //! ![lines](https://raw.githubusercontent.com/gringasalpastor/mule-map/refs/heads/master/assets/lines.svg)
+//!
+//! ## License
+//!
+//! Licensed under either of:
+//!
+//!  * Apache License, Version 2.0, ([LICENSE-APACHE](LICENSE-APACHE) or [https://www.apache.org/licenses/LICENSE-2.0](https://www.apache.org/licenses/LICENSE-2.0))
+//!  * MIT license ([LICENSE-MIT](LICENSE-MIT) or [https://opensource.org/licenses/MIT](https://opensource.org/licenses/MIT))
+//!
+//! at your option.
+//!
 
 pub use crate::mule_map::entry::*;
 pub use crate::mule_map::*;
--- src/mule_map/iterators.rs
+++ src/mule_map/iterators.rs
@@ -1,20 +1,12 @@
-use crate::mule_map::key_index::KeyIndex;
 use crate::MuleMap;
+use crate::mule_map::key_index::KeyIndex;
 
 use num_traits::AsPrimitive;
 use num_traits::PrimInt;
 use std::fmt::Debug;
 
-// struct Iter {}
-
-impl<
-        K,
-        V,
-        S,
-        const ZERO_IS_SENTINEL: bool,
-        const TABLE_MIN_VALUE: i128,
-        const TABLE_MAX_VALUE: i128,
-    > MuleMap<K, V, S, ZERO_IS_SENTINEL, TABLE_MIN_VALUE, TABLE_MAX_VALUE>
+impl<K, V, S, const TABLE_MIN_VALUE: i128, const TABLE_SIZE: usize>
+    MuleMap<K, V, S, TABLE_MIN_VALUE, TABLE_SIZE>
 where
     K: PrimInt + Eq + std::hash::Hash + KeyIndex<K, TABLE_MIN_VALUE> + TryFrom<i128> + 'static,
     S: Default + std::hash::BuildHasher,
@@ -22,48 +14,15 @@ where
     i128: AsPrimitive<K>,
     <K as TryFrom<i128>>::Error: Debug,
 {
-    fn _iter(
-        &self,
-    ) -> impl Iterator<Item = K> + use<'_, K, V, S, ZERO_IS_SENTINEL, TABLE_MIN_VALUE, TABLE_MAX_VALUE>
+    fn _iter(&self) -> impl Iterator<Item = V> + '_
     where
         usize: AsPrimitive<K>,
     {
-        self.hash_map.keys().copied().chain(
-            self.occupied_map
+        self.hash_map.values().cloned().chain(
+            self.table
                 .iter()
-                .enumerate()
-                .filter_map(|(index, occupied)| if *occupied { Some(index.as_()) } else { None }),
+                .filter_map(|value| value.as_ref())
+                .cloned(),
         )
     }
 }
-
-// pub fn keys(
-//     &self,
-// ) -> impl Iterator<Item = K> + use<'_, K, V, S, ZERO_IS_SENTINEL, TABLE_MIN_VALUE, TABLE_MAX_VALUE>
-// where
-//     usize: AsPrimitive<K>,
-// {
-//     if !ZERO_IS_SENTINEL {
-//         self.hash_map.keys()
-//     } else {
-//         // NOTE: !!!! need to convert index back to key
-//         self.hash_map.keys().map(|&key| key).chain(
-//             self.occupied_map
-//                 .iter()
-//                 .enumerate()
-//                 .filter_map(
-//                     |(index, occupied)| if *occupied { Some(index.as_()) } else { None },
-//                 ),
-//         )
-//     }
-
-//     // if !ZERO_IS_SENTINEL{
-//     // self.hash_map.keys().map(|&key| key).chain(
-//     //     self.occupied_map
-//     //         .iter()
-//     //         .enumerate()
-//     //         .filter(|(index, occupied): (usize, bool)| occupied)
-//     //         .map(|(index, occupied): (usize, bool)| index),
-//     // )
-//     //}
-// }
